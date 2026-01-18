053d2fb0b64c788d76a971d4ac213aa6ea115975
Guilherme Giusti <guigiusti@proton.me>
2026-01-17 21:34:44 -0300

    refactor: improve build script to allow nested paths

--- build.py
+++ build.py
@@ -1,15 +1,25 @@
 import json
 import os
 
-with open('redirects.json', 'r') as file:
+with open("redirects.json", "r") as file:
     redirects = json.load(file)
 
-with open('base.html', 'r') as file:
+with open("base.html", "r") as file:
     base_html = file.read()
 
-os.makedirs('public', exist_ok=True)
+os.makedirs("public", exist_ok=True)
 
-for redirect in redirects['redirects']:
-    file_name = str("index" if "/" == redirect['location'] else redirect['location']).replace("/", "")
-    with open(f'public/{file_name}.html', 'w') as file:
-        file.write(base_html.format(url=redirect['redirect'], title=redirects['title'].format(page=file_name.capitalize())))
+for redirect in redirects["redirects"]:
+    location = (
+        "index" if "/" == redirect["location"] else redirect["location"].lstrip("/")
+    )
+    file_path = os.path.join("public", location.lower() + ".html")
+    page_name = location.split("/")[-1].capitalize()
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, "w") as file:
+        file.write(
+            base_html.format(
+                url=redirect["redirect"],
+                title=redirects["title"].format(page=page_name),
+            )
+        )
